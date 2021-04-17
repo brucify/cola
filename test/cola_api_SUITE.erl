ed1ccaeda0a00a00f5e7db507732d54fd5aa19bb
Bruce Yinhe <bruce.yinhe@gmail.com>
2021-04-17 13:19:07 +0200

    katt test

--- test/cola_api_SUITE.erl
+++ test/cola_api_SUITE.erl
@@ -13,10 +13,13 @@
 -compile(export_all).
 -endif.
 
+-define(API_URL, "https://127.0.0.1:8443").
+
 %%%===================================================================
 %%% ct_suite callbacks
 %%%===================================================================
-all() -> lists:flatten([ group(1)
+
+all() -> lists:flatten([ group(rooms)
                        ]).
 
 init_per_suite(Config) ->
@@ -37,9 +40,19 @@ end_per_testcase(_TestCase, _Config) ->
 %%% Tests
 %%%===================================================================
 
-group(1) ->
-  [ get_rooms_coke
+group(rooms) ->
+  [ get_rooms_pepsi
+  , get_rooms_coke
   ].
+
+get_rooms_coke(Config0) ->
+  Config1 = [ {hackney_options, hackney_options(coke, Config0)} | Config0],
+  run("get_rooms_coke.apib", Config1).
+
+get_rooms_pepsi(Config0) ->
+  Config1 = [ {hackney_options, hackney_options(pepsi, Config0)} | Config0],
+  run("get_rooms_pepsi.apib", Config1).
+
 
 %%%===================================================================
 %%% Internal functions
@@ -48,28 +61,37 @@ group(1) ->
 run(BlueprintFilePath, Config0) ->
   Config1 = [X || {_K,V}=X <- Config0, not is_function(V), not is_reference(V), not is_pid(V)],
   DataDir = proplists:get_value(data_dir, Config1),
-  Config2 = [ {base_url, "https://127.0.0.1:8443"} | Config1],
   Path = filename:join(DataDir, BlueprintFilePath),
+  Config2 = [ {base_url, ?API_URL} | Config1],
   case katt:run(Path, Config2) of
     {error, _Reason, _Details}=RunError                                  -> ct:fail(RunError);
     {fail, _Filename, _Config, _FinalConfig, _TransactionResults}=Result -> ct:fail(Result);
     {pass, _Filename, _Config, FinalConfig, _TransactionResults}         -> FinalConfig
   end.
 
-%%%===================================================================
-%%% Tests
-%%%===================================================================
-
-get_rooms_coke(Config0) ->
-  Config1 = [ {request_options, [{ssl_options, [ {certfile, coke_cert(Config0)}
-                                               , {keyfile,  coke_key(Config0)}
-                                               ]}]}
-              | Config0
-            ],
-  run("get_rooms_coke.apib", Config1).
+hackney_options(coke, Config) ->
+  [ {ssl_options, [ {certfile, coke_cert(Config)}
+                  , {keyfile,  coke_key(Config)}
+                  ]}
+  , {insecure, false}
+  , {pool,     false}
+  ];
+hackney_options(pepsi, Config) ->
+  [ {ssl_options, [ {certfile, pepsi_cert(Config)}
+                  , {keyfile,  pepsi_key(Config)}
+                  ]}
+  , {insecure, false}
+  , {pool,     false}
+  ].
 
 coke_cert(Config) ->
   filename:join(proplists:get_value(data_dir, Config),"coke.crt").
 
 coke_key(Config) ->
   filename:join(proplists:get_value(data_dir, Config),"coke.key").
+
+pepsi_cert(Config) ->
+  filename:join(proplists:get_value(data_dir, Config),"pepsi.crt").
+
+pepsi_key(Config) ->
+  filename:join(proplists:get_value(data_dir, Config),"pepsi.key").
