c996e39dd360a41ea0bdd0111dc07825907b872b
Bruce Yinhe <bruce.yinhe@gmail.com>
2021-04-18 11:04:49 +0200

    Add tests

--- test/cola_api_SUITE.erl
+++ test/cola_api_SUITE.erl
@@ -20,6 +20,7 @@
 %%%===================================================================
 
 all() -> lists:flatten([ group(rooms)
+                       , group(bookings)
                        ]).
 
 init_per_suite(Config) ->
@@ -43,15 +44,26 @@ end_per_testcase(_TestCase, _Config) ->
 group(rooms) ->
   [ get_rooms_pepsi
   , get_rooms_coke
+  ];
+group(bookings) ->
+  [ post_bookings_coke
   ].
 
 get_rooms_coke(Config0) ->
-  Config1 = [ {hackney_options, hackney_options(coke, Config0)} | Config0],
+  Config1 = [ coke(Config0) | Config0],
   run("get_rooms_coke.apib", Config1).
 
 get_rooms_pepsi(Config0) ->
-  Config1 = [ {hackney_options, hackney_options(pepsi, Config0)} | Config0],
+  Config1 = [ pepsi(Config0) | Config0],
   run("get_rooms_pepsi.apib", Config1).
+
+post_bookings_coke(Config0) ->
+  Config1 = [ coke(Config0) | Config0],
+  _       = run("get_rooms_coke.apib", Config1),
+  Config2 = run("post_bookings_coke.apib", Config1),
+  _       = run("get_rooms_coke_2.apib", Config2),
+  _       = run("get_bookings_id_coke.apib", Config2),
+  ?assertEqual(true, uuid:is_valid(proplists:get_value(booking_id, Config2))).
 
 
 %%%===================================================================
@@ -66,8 +78,13 @@ run(BlueprintFilePath, Config0) ->
   case katt:run(Path, Config2) of
     {error, _Reason, _Details}=RunError                                  -> ct:fail(RunError);
     {fail, _Filename, _Config, _FinalConfig, _TransactionResults}=Result -> ct:fail(Result);
-    {pass, _Filename, _Config, FinalConfig, _TransactionResults}         -> FinalConfig
+    {pass, _Filename, _Config, FinalConfig, _TransactionResults}         -> [{list_to_atom(K),V}||{K,V} <- FinalConfig]
+                                                                            ++ (Config0 -- Config1)
   end.
+
+coke(Config) -> {hackney_options, hackney_options(coke, Config)}.
+
+pepsi(Config) -> {hackney_options, hackney_options(pepsi, Config)}.
 
 hackney_options(coke, Config) ->
   [ {ssl_options, [ {certfile, coke_cert(Config)}
